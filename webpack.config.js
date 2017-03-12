bf87ab8ae501470a5e163aa27a546725e9d9762b
Joseph Yølk Chiocchi <joe@yolk.cc>
2017-03-12 16:16:59 -0500

    update webpack plugins, really only needed when we’re `NODE_ENV=production`

--- webpack.config.js
+++ webpack.config.js
@@ -8,30 +8,31 @@ const S3Plugin = require("webpack-s3-plugin");
 
 function getPlugins() {
   let plugins = [];
-  plugins.push(new
-    ArchivePlugin({
-      output: 'breakfast-bell',
-      format: 'zip'
-    })
-  );
-  plugins.push(new
-    S3Plugin({
-      include: /(.*\.zip|swagger.yaml)/,
-      s3Options: {
-        region: 'us-east-1'
-      },
-      s3UploadOptions: {
-        Bucket: 'yolks-breakfasthouse-lambda'
-      },
-      directory: __dirname,
-    })
-  );
+
   if (process.env.NODE_ENV === 'production') {
     plugins.push(new
       webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false,
         }
+      })
+    );
+    plugins.push(new
+      ArchivePlugin({
+        output: 'breakfast-bell',
+        format: 'zip'
+      })
+    );
+    plugins.push(new
+      S3Plugin({
+        include: /(.*\.zip|swagger.yaml)/,
+        s3Options: {
+          region: 'us-east-1'
+        },
+        s3UploadOptions: {
+          Bucket: 'yolks-breakfasthouse-lambda'
+        },
+        directory: __dirname,
       })
     );
   }
