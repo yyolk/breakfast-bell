3fb096da8e89e58828f03ac10222d0d47fa0bed2
Joseph Yølk Chiocchi <joe@yolk.cc>
2017-07-30 16:09:50 -0500

    rename to hellotwiml

--- sms/handler.js
+++ sms/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.helloxml = (event, context, callback) => {
+module.exports.hellotwiml = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
